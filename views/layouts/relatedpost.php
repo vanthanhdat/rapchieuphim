28ec0fd1af0aa3c87a6f7c14c9c147bf740d9939
vanthanhdat <vanthanhdat720@gmail.com>
2018-08-25 16:12:49 +0700

    datdat xoa 1 dong

--- views/layouts/relatedpost.php
+++ views/layouts/relatedpost.php
@@ -15,7 +15,7 @@ use yii\bootstrap\Tabs;
             <li><i class="fa fa-arrow-right"></i>&nbsp;<a href="/binh-luan-phim/preview-the-nun-khoi-nguon-moi-toi-ac">[Preview] The Nun: Khởi Nguồn Mọi Tội Ác</a></li>
             
             <li><i class="fa fa-arrow-right"></i>&nbsp;<a href="/binh-luan-phim/preview-the-nun-khoi-nguon-moi-toi-ac">[Preview] The Nun: Khởi Nguồn Mọi Tội Ác</a></li>
-            <li><i class="fa fa-arrow-right"></i>&nbsp;<a href="/binh-luan-phim/preview-the-nun-khoi-nguon-moi-toi-ac">[Preview] The Nun: Khởi Nguồn Mọi Tội Ác</a></li>
+           
         </ul>
     </div>
 </section>
