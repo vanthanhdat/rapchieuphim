c10cf34345c309e9af1d695c5908e4c0773707b7
vanthanhdat <vanthanhdat720@gmail.com>
2018-08-27 13:48:42 +0700

    con cac ne

--- modules/admin/controllers/TheloaiController.php
+++ modules/admin/controllers/TheloaiController.php
@@ -252,6 +252,7 @@ class TheloaiController extends Controller
         andWhere(['lichchieu.idphim' => $id])->all();
         $page = explode('page=',Yii::$app->request->referrer);
         $test = '';
+        $test1 = '';
         if (empty($check) && $phim->status !== 1) {
             $phim->status = 0;
             if ($phim->save()) {
